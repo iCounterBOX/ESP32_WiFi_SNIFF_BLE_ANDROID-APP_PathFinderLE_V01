d6061f89764a006f1f4de98d33fcd174dace272f
Kina <kristinas_mail@gmx.de>
2020-04-21 09:58:27 +0200

    draft
    This is NOT finished!!
    First version of esp32 BLE sending some Data!!

---
+++ EXAMPLES/ESP32_bleWrite2Mobile/Release/BLE/BLEExceptions.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\kristina\AppData\Local\Temp\VMBuilds\ESP32_bleWrite2Mobile\esp32_esp32\Release\BLE\BLEExceptions.cpp.o: \
+ C:\Users\kristina\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.4\libraries\BLE\src\BLEExceptions.cpp
